dfd7a84f03bb150c4e0daa454c0eb71a8c896e77
MareK985 <59637752+MareK985@users.noreply.github.com>
2020-05-19 19:35:20 +0200

    Language Menu setup

--- components/LangNavigation.vue
+++ components/LangNavigation.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
-    <nuxt-link :to="switchLocalePath('sl')">Slovensko</nuxt-link>
-    <nuxt-link :to="switchLocalePath('en')">English</nuxt-link>
+    <div class="langMenu">
+      <nuxt-link :to="switchLocalePath('sl')" class="language">SL</nuxt-link>
+      <nuxt-link :to="switchLocalePath('en')" class="language">EN</nuxt-link>
+    </div>
   </div>
 </template>
 
@@ -9,4 +11,21 @@
 export default {};
 </script>
 
-<style lang="css" scoped></style>
+<style lang="css">
+.langMenu {
+  padding: 20px;
+  position: absolute;
+  right: 0;
+}
+.language {
+  color: white;
+  font-size: 1.8em;
+  margin: 0.5em;
+}
+@media screen and (max-width: 680px) {
+  .language {
+    font-size: 1em;
+    margin: 0.2em;
+  }
+}
+</style>
